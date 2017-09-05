ae691ebe77cd61bfb06b46fc08af32e5bd85a9f0
matt-kendall <mattk91@gmail.com>
2017-09-05 18:21:10 +0100

    Challenge 0

---
+++ challenge-0.py
@@ -0,0 +1,8 @@
+"""
+Python Challenge Level 0.
+
+On the screen is a monitor with a post-it note containing the text '2^38'.
+"""
+print(2**38)
+# 274877906944
+# http://www.pythonchallenge.com/pc/def/274877906944.html is the next URL.
