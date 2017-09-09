c121762ff61e2e83c60f58568fab8eb36650d305
matt-kendall <mattk91@gmail.com>
2017-09-09 11:30:36 +0100

    Style changes to challenge 1

--- challenge-1.py
+++ challenge-1.py
@@ -16,15 +16,18 @@ HINT = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc " \
        "dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr " \
        "gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. " \
        "lmu ynnjw ml rfc spj."
+ALPHA_SHIFT = 2  # The clue implies a caesar cipher of shift 2.
 
-# The clue implies a caesar cipher of shift 2.
 
 def shift_alphabet(string, shift):
+    """
+    Shift an input strings letters through the alphabet by a specified shift.
+    """
     old_map = ascii_lowercase
     new_map = old_map[shift:] + old_map[:shift]
     return string.translate(str.maketrans(old_map, new_map))
 
-ALPHA_SHIFT = 2
+
 print(shift_alphabet(HINT, ALPHA_SHIFT))
 # 'i hope you didnt translate it by hand. thats what computers are for.
 #  doing it in by hand is inefficient and that's why this text is so long.
